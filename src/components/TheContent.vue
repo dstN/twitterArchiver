41fb5422e32b1328dbf1d147e4bafe5523524312
PsclDev <me@pscl.dev>
2022-11-25 00:10:33 +0100

    resolve #3 by tweet id and content

--- src/components/TheContent.vue
+++ src/components/TheContent.vue
@@ -1,15 +1,50 @@
 <script setup>
-import { toRefs, computed } from "vue";
+import { ref, toRefs, computed } from "vue";
 import Tweet from "./partials/Tweet.vue";
+import debounce from "lodash/debounce";
 
 const props = defineProps({
   data: Object,
 });
 
 const { data } = toRefs(props);
+const filteredData = ref(data.value.tweets);
+
+const searchTerm = ref("");
+const searchTermCaseSensitive = ref(false);
+
+const onSearchTermChange = debounce(() => {
+  filterTweets();
+}, 150);
+
+const searchTermCaseSensitiveChanged = () => {
+  filterTweets();
+};
+
+function filterTweets() {
+  if (searchTerm.value.length < 3) return;
+
+  const insensitiveFilter = (tweet) => {
+    return (
+      tweet.full_text.toLowerCase().includes(searchTerm.value.toLowerCase()) ||
+      tweet.id === searchTerm.value
+    );
+  };
+
+  const sensitiveFilter = (tweet) => {
+    return (
+      tweet.full_text.includes(searchTerm.value) ||
+      tweet.id === searchTerm.value
+    );
+  };
+
+  filteredData.value = data.value.tweets.filter(
+    searchTermCaseSensitive.value ? sensitiveFilter : insensitiveFilter
+  );
+}
 
 const tweets = computed(() => {
-  return data.value.tweets;
+  return filteredData.value;
 });
 
 const user = computed(() => {
@@ -18,29 +53,54 @@ const user = computed(() => {
 </script>
 
 <template>
-  <DynamicScroller
-    :items="tweets"
-    :min-item-size="54"
-    class="scroller mx-auto min-h-screen max-w-screen-sm shadow-2xl"
-    page-mode
-  >
-    <template v-slot="{ item, index, active }">
-      <DynamicScrollerItem
-        :item="item"
-        :active="active"
-        :size-dependencies="[item.full_text]"
-        :data-index="index"
+  <div class="scroller mx-auto min-h-screen max-w-screen-sm shadow-2xl">
+    <div class="flex gap-5">
+      <input
+        class="my-5 w-full"
+        type="text"
+        placeholder="Search..."
+        v-model="searchTerm"
+        @input="onSearchTermChange"
+      />
+      <div>
+        <input
+          type="checkbox"
+          id="checkbox"
+          v-model="searchTermCaseSensitive"
+          @change="searchTermCaseSensitiveChanged"
+        />
+        <label for="checkbox">Case-Sensitive</label>
+      </div>
+    </div>
+
+    <template v-if="tweets.length > 0">
+      <DynamicScroller
+        :items="tweets"
+        :min-item-size="54"
+        page-mode
       >
-        <div
-          :key="item.id"
-          class="flex justify-center"
-        >
-          <Tweet
-            :data="item"
-            :user="user"
-          />
-        </div>
-      </DynamicScrollerItem>
+        <template v-slot="{ item, index, active }">
+          <DynamicScrollerItem
+            :item="item"
+            :active="active"
+            :size-dependencies="[item.full_text]"
+            :data-index="index"
+          >
+            <div
+              :key="item.id"
+              class="flex justify-center"
+            >
+              <Tweet
+                :data="item"
+                :user="user"
+              />
+            </div>
+          </DynamicScrollerItem>
+        </template>
+      </DynamicScroller>
     </template>
-  </DynamicScroller>
+    <template v-else>
+      <h2>No tweets found by given search term: {{ searchTerm }}</h2>
+    </template>
+  </div>
 </template>
