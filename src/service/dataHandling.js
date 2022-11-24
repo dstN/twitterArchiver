bd083d9fb1df254ea09874e0adaec5f972ba8976
dstN <github@yinside.de>
2022-11-24 21:52:43 +0100

    Old State

--- src/service/dataHandling.js
+++ src/service/dataHandling.js
@@ -11,21 +11,30 @@ async function fixJSON(json) {
 async function tweetHandler(data) {
   const regex =
     /(https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|www\.[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9]+\.[^\s]{2,}|www\.[a-zA-Z0-9]+\.[^\s]{2,})/gm;
-  for (let tweet of data) {
+  for (let [tweet, index] of data) {
+    console.log(index);
     const result = tweet.full_text.matchAll(regex);
     for (const match of result) {
       const url = match[0];
       const entitiesUrls = tweet.entities.urls;
+      const extendedEntities = tweet.extended_entities;
+      //if (entitiesUrls.length && extendedEntities != undefined) {
+
+      //}
       if (entitiesUrls.length) {
         // URLs exist - is NOT Media
         for (const entity of entitiesUrls) {
           if (url === entity.url) {
-            tweet.full_text = tweet.full_text.replaceAll(
-              url,
-              entity.expanded_url
-            );
             if (tweet.extended_entities != undefined) {
+              // URL exist - AND has media
+              tweet.full_text = tweet.full_text.replaceAll(url, "");
               console.log(tweet.extended_entities);
+            } else {
+              // URL exist ONLY
+              tweet.full_text = tweet.full_text.replaceAll(
+                url,
+                entity.expanded_url
+              );
             }
           }
         }
